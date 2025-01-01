dfc9f111f7c583f948bb4f0bb6a36059ed0323ff
chivy140820a <chivy1408@gmail.com>
2025-01-01 22:21:16 +0700

    update

--- DemoCustomCICD/Controllers/WeatherForecastController.cs
+++ DemoCustomCICD/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DemoCustomCICD.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing"
+            "Freezing","Demo1"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
